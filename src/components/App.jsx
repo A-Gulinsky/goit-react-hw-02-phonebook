00b4bb290ce136602ca4acd2cf8f9e0ece8338f9
Alexandr Gulinsky <swith0711@gmail.com>
2023-08-01 18:53:26 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,71 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react"
+import shortid from "shortid"
+
+import Container from "./App/App.styled"
+import Section from "./Section"
+
+import Phonebook from "./Phonebook"
+import Contacts from "./Contacts"
+import Filter from "./Filter"
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: ''
+  }
+
+  addNewContact = (name,number) => {
+
+    const newId = shortid.generate()
+
+    const newContact = {
+      id: newId,
+      name,
+      number
+    }
+
+    if (this.state.contacts.some(contact => name === contact.name)) {
+      return alert(`${name} is already in contacts`)
+    }
+
+    this.setState(({ contacts }) => ({
+      contacts: [newContact,...contacts]
+    }))
+  }
+
+  filterContacts = e => {
+    this.setState({filter: e.currentTarget.value})
+  }
+
+  getFilterContacts = () => {
+    const { filter, contacts } = this.state
+    
+    const normalizedFilter = filter.toLowerCase()
+
+    return contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter))
+  }
+
+  deleteContact = contactId => {
+
+    this.setState(({contacts}) => ({
+      contacts: contacts.filter(contact => contact.id !== contactId)
+    }))
+  }
+
+  render() {
+    const visibleContacts = this.getFilterContacts()
+    return (
+      <Container>
+        <Section title="Phonebook">
+          <Phonebook onSubmit={this.addNewContact}></Phonebook>
+        </Section>
+        <Section title="Contacts">
+          <Filter onChange={this.filterContacts}></Filter>
+          <Contacts contacts={visibleContacts} onDeleteContact={this.deleteContact}></Contacts>
+        </Section>
+      </Container>
+    )
+  }
+}
+
+export default App
